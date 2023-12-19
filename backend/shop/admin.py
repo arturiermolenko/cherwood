73aa769837e236a088d8ae437c20c173ffb777fd
arturiermolenko <arturiermolenko@gmail.com>
2023-12-19 11:12:36 +0100

    registered models in admin.py

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, ProductImage
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "length",
+        "width",
+        "height",
+        "material",
+        "coating",
+        "additional_info"
+    )
+    list_filter = ("name", "material", "coating")
+    search_fields = ("name",)
+
+
+admin.site.register(ProductImage)

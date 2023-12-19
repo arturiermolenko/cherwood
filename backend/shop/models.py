ee646a0fe1696399e747f5279f9182a584bc2542
arturiermolenko <arturiermolenko@gmail.com>
2023-12-19 11:12:16 +0100

    created Category, Product and ProductImage models. created serializers.py - ToDo. added views - ToDo

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,3 +1,55 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.db.models import CharField
+
+
+def file_path(instance, filename, suffix, folder) -> str:
+    _, ext = os.path.splitext(filename)
+    filename = f"{suffix}-{uuid.uuid4()}{ext}"
+    return os.path.join(f"uploads/images/{folder}", filename)
+
+
+def category_image_file_path(instance, filename) -> str:
+    return file_path(instance, filename, instance.name, "categories")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    image = models.ImageField(null=True, upload_to=category_image_file_path)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "categories"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    length = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    material = models.CharField(max_length=255)
+    coating = models.CharField(max_length=255)
+    additional_info = models.CharField(max_length=500)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> CharField:
+        return self.name
+
+
+def product_image_file_path(instance, filename) -> str:
+    return file_path(instance, filename, instance.product.name, "products")
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to=product_image_file_path)
+    product = models.ForeignKey(
+        "Product", on_delete=models.CASCADE, related_name="images"
+    )
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from shop.models import Category, Product
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()

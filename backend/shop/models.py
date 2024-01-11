9080bd1c48a640372b19b759e74f75804a8b1a0f
Olexander Bryl <edlrian814@gmail.com>
2024-01-11 20:44:06 +0200

    Simplify Order model & move it to shop app

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 
+from django.conf import settings
 from django.db import models
 
 
@@ -87,3 +88,13 @@ class ProductImage(models.Model):
     product = models.ForeignKey(
         "Product", on_delete=models.CASCADE, related_name="images"
     )
+
+
+class Order(models.Model):
+    user = models.EmailField(max_length=255)
+    products = models.ManyToManyField(Product, related_name="orders")
+    created_at = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.user}, {self.created_at}, total: {self.total}"

3b312482723638cfc6edace06e010a9abcabd62a
Olexander Bryl <edlrian814@gmail.com>
2024-01-12 12:44:17 +0200

    Add OrderItem model

--- backend/order/models.py
+++ backend/order/models.py
@@ -14,3 +14,23 @@ class Order(models.Model):
 
     def __str__(self) -> str:
         return f"{self.email}, {self.created_at}"
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(
+        "shop.Product", on_delete=models.CASCADE, related_name="order_items"
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="order_items"
+    )
+
+    def calculate_total(self) -> Decimal:
+        return self.product.price
+
+    def __str__(self) -> str:
+        return f"{self.product} X {self.quantity}"

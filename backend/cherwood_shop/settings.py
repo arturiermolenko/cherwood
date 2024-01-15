d8377401674f244904de0720b6de2f3f08e22fce
arturiermolenko <arturiermolenko@gmail.com>
2024-01-15 22:24:46 +0100

    migratios fixed

--- backend/cherwood_shop/settings.py
+++ backend/cherwood_shop/settings.py
@@ -178,7 +178,7 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # app password preferabl
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
-    "http://127.0.0.1:8000/",
+    "http://127.0.0.1:8000",
     "http://localhost:3000",
 ]
 CORS_ALLOW_ALL_HEADERS = True

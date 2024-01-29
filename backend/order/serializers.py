641b2ff6f054b0476a409feb632383e2592fb44d
Olexander Bryl <edlrian814@gmail.com>
2024-01-29 16:19:57 +0200

    Make it possible to send regions names both in Ukrainian and English

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -1,10 +1,13 @@
+from django.conf import settings
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from order.models import Order
 
 
 class OrderCreateSerializer(serializers.ModelSerializer):
     email = serializers.EmailField()
+    region = serializers.CharField(max_length=65)
 
     class Meta:
         model = Order
@@ -16,6 +19,15 @@ class OrderCreateSerializer(serializers.ModelSerializer):
             "region",
             "city"
         )
+
+    def validate_region(self, value):
+        region_dict = settings.REGIONS_DICT
+        if value in list(region_dict.keys()):
+            return value
+        elif value in list(region_dict.values()):
+            position = list(region_dict.values()).index(value)
+            return list(region_dict.keys())[position]
+        raise ValidationError(f"{value} is not a possible choice.")
 
 
 class OrderListSerializer(serializers.ModelSerializer):
